fe0ac515b1791ababa9fa2b830cce30992980d0a
ReesRingrose <c15388176@mydit.ie>
2018-02-14 16:51:44 +0000

    update

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
@@ -24,6 +27,39 @@ public class Main {
     public static class TuneBook
     {
         ArrayList<String> Tunes = new ArrayList<String>();
+
+
+
+         public void TuneBooks()
+        {
+            BufferedReader inputStream = null;
+            try {
+                inputStream = new BufferedReader(new FileReader("hnj0.abc.txt"));
+
+                String l;
+                while ((l = inputStream.readLine()) != null)
+                {
+                    Tunes.add(l);
+                }
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                if (inputStream != null) {
+                    try
+                    {
+                        inputStream.close();
+                    }
+                    catch(Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
 
     }
 }
